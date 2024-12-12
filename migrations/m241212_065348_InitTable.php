1950ae8153043cd591dbde26266b37f8148aa679
Evgenia Frolova <evgenia.frolova2018@gmail.com>
2024-12-12 22:33:32 +0300

    первый коммит

---
+++ migrations/m241212_065348_InitTable.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m241212_065348_InitTable
+ */
+class m241212_065348_InitTable extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m241212_065348_InitTable cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m241212_065348_InitTable cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
